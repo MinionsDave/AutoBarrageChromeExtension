5f79984890ce5aaa3ab7746d90ff1baa48b407e9
wudengguang <651882883@qq.com>
2016-12-02 00:33:41 +0800

    简化下代码

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // 先把视频隐藏了
-var i = 0;
-setInterval(function () {
-    i++;
-    $('.room-chat-texta').val('gaylun.tv 直播整合平台' + i);
-    $('.room-chat-send').click();
-}, 10000);
+var i = 0,
+    msg = 'gaylun.tv 直播整合平台',
+    href = window.location.href;
+
+
+if (href.indexOf('quanmin') > 0) {
+    sendBarrage('.w-sender_textarea', '.w-sender_submit-btn');
+} else if (href.indexOf('panda') > 0) {
+    sendBarrage('.room-chat-texta', '.room-chat-send');
+} else if (href.indexOf('douyu') > 0) {
+    sendBarrage('.cs-textarea', '.b-btn');
+}
+
+function sendBarrage(textareaSelector, sendBtnSelector) {
+    setInterval(function () {
+        i++;
+        $(textareaSelector).val(msg + i);
+        $(sendBtnSelector).click();
+    }, 20000);
+}
